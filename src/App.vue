af8c80d2d3cc98bc81d86a48a289b9c9da16cddc
ProgerFox <astlogastafiev@yandex.ru>
2022-12-21 19:53:42 +0300

    [add] ChatContainer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChatContainer></ChatContainer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatContainer from './components/ChatContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatContainer
   }
 }
 </script>
